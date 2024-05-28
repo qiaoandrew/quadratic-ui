b59633de37777b3992e6d42c65072600f721672a
Andrew Qiao <andrewqiao2004@gmail.com>
2024-05-27 18:45:09 -0700

    remove unnecessary cn in pagination

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -40,7 +40,7 @@ const PaginationItem = React.forwardRef<
   HTMLLIElement,
   React.ComponentProps<"li">
 >(({ className, ...props }, ref) => (
-  <li ref={ref} className={cn("", className)} {...props} />
+  <li ref={ref} className={className} {...props} />
 ));
 PaginationItem.displayName = "PaginationItem";
 
