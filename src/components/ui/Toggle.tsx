e4cc3da5a0d9ffbf4ed0fe946ef0fbbaa37a59b9
Andrew Qiao <andrewqiao2004@gmail.com>
2024-05-29 19:35:52 -0700

    make dark accent color different than border color

--- src/components/ui/Toggle.tsx
+++ src/components/ui/Toggle.tsx
@@ -22,8 +22,8 @@ const toggleVariants = cva(
           "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-9 px-2.5",
         sm: "h-8 px-2",
+        default: "h-9 px-2.5",
         lg: "h-10 px-3",
       },
     },
