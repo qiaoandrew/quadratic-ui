7d5ffbdf30cc5844edd7ebb573aeda963d12b369
Andrew Qiao <andrewqiao2004@gmail.com>
2024-06-20 19:14:16 -0700

    make select ring smaller

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -21,7 +21,7 @@ const SelectTrigger = React.forwardRef<
     className={cn(
       "flex w-full items-center justify-between rounded-2 border border-input bg-background px-3 py-2.5 text-3.5 ring-offset-background",
       "placeholder:text-muted-foreground",
-      "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+      "focus:outline-none focus:ring-1 focus:ring-ring focus:ring-offset-1",
       "disabled:cursor-not-allowed disabled:opacity-50",
       "[&>span]:line-clamp-1 [&>span]:text-left",
       className,
