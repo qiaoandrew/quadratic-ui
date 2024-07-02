fe9c067f392e9dbb2bd7ced902b4e92e7c9fee60
Andrew Qiao <andrewqiao2004@gmail.com>
2024-07-01 20:12:54 -0700

    more adjustments

--- src/app/docs/_components/demos/input/InputDemo.tsx
+++ src/app/docs/_components/demos/input/InputDemo.tsx
@@ -1,5 +1,5 @@
 import { Input } from "~/components/ui/Input";
 
 export default function InputDemo() {
-  return <Input type="email" placeholder="Email" className="max-w-[320px]" />;
+  return <Input type="email" placeholder="Email" className="max-w-80" />;
 }
