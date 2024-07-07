b0699868d37905562e2c049534b15c7f7366f57e
Andrew Qiao <andrewqiao2004@gmail.com>
2024-07-06 17:13:05 -0700

    fix item is possible undefined error

--- src/components/ui/Chart.tsx
+++ src/components/ui/Chart.tsx
@@ -136,6 +136,11 @@ const ChartTooltipContent = React.forwardRef<
       }
 
       const [item] = payload;
+
+      if (!item) {
+        return null;
+      }
+
       const key = `${labelKey ?? item.dataKey ?? item.name ?? "value"}`;
       const itemConfig = getPayloadConfigFromPayload(config, item, key);
       const value =
