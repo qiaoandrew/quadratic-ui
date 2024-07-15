29f1ff140be079ed9c14f60b1ae0f523c3bacc69
Andrew Qiao <andrewqiao2004@gmail.com>
2024-07-14 17:44:24 -0700

    data table uptime color tweak

--- src/app/docs/_components/demos/data-table/DataTableDemo.tsx
+++ src/app/docs/_components/demos/data-table/DataTableDemo.tsx
@@ -514,9 +514,9 @@ function UptimeCell({ uptime }: { uptime: number | null }) {
       {uptime !== null && (
         <span className="flex items-center justify-between gap-x-1 font-semibold">
           {uptime}%
-          <span className="relative h-4 w-20 bg-success">
+          <span className="relative h-4 w-20 bg-success-foreground">
             <span
-              className="absolute inset-y-0 right-0 bg-destructive"
+              className="absolute inset-y-0 right-0 bg-destructive-foreground"
               style={{ width: failureWidth }}
             />
           </span>
