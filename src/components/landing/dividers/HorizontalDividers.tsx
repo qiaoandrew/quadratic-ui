e52fc185c312c6a46749a1cfef13052e33f96d42
Andrew Qiao <andrewqiao2004@gmail.com>
2024-12-31 16:33:15 -0500

    programmatically show dividers

--- src/components/landing/dividers/HorizontalDividers.tsx
+++ src/components/landing/dividers/HorizontalDividers.tsx
@@ -1,9 +1,22 @@
+import { cn } from "~/utils/tailwind";
+import { LANDING_DEMOS } from "~/constants/landingDemos";
+
 export default function HorizontalDividers() {
+  const twoColumnDividerCount = Math.ceil(LANDING_DEMOS.length / 2);
+  const threeColumnDividerCount = Math.ceil(LANDING_DEMOS.length / 3);
+
   return (
     <div className="pointer-events-none absolute inset-x-0 inset-y-7 z-20 flex flex-col gap-y-16">
-      <div className="h-64 border-y" />
-      <div className="h-64 border-y" />
-      <div className="h-64 border-y" />
+      {Array.from({ length: LANDING_DEMOS.length }).map((_, i) => (
+        <div
+          className={cn(
+            "h-64 border-y",
+            i >= twoColumnDividerCount && "md:hidden",
+            i >= threeColumnDividerCount && "3xl:hidden",
+          )}
+          key={i}
+        />
+      ))}
     </div>
   );
 }
--- src/components/landing/dividers/VerticalDividers.tsx
+++ src/components/landing/dividers/VerticalDividers.tsx
@@ -1,9 +1,17 @@
+import { cn } from "~/utils/tailwind";
+
 export default function VerticalDividers() {
   return (
-    <div className="pointer-events-none absolute inset-x-6 inset-y-0 z-20 grid md:grid-cols-2 md:gap-x-8 3xl:inset-x-[calc((100vw-1248px)/2)] 3xl:grid-cols-3">
+    <div
+      className={cn(
+        "pointer-events-none absolute inset-x-6 inset-y-0 grid",
+        "md:grid-cols-2 md:gap-x-8 3xl:inset-x-[calc((100vw-1248px)/2)]",
+        "3xl:grid-cols-3",
+      )}
+    >
       <div className="border-x" />
-      <div className="border-x" />
-      <div className="border-x" />
+      <div className="hidden border-x md:block" />
+      <div className="hidden border-x 3xl:block" />
     </div>
   );
 }
