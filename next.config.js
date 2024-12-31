540ca7757b0483e67bc9c32603f5f16e9fc1da3c
Andrew Qiao <andrewqiao2004@gmail.com>
2024-12-31 16:39:16 -0500

    install next mdx

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import createMDX from "@next/mdx";
+
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  pageExtensions: ["ts", "tsx", "mdx"],
+};
 
-export default config;
+const withMdx = createMDX();
+export default withMdx(config);
