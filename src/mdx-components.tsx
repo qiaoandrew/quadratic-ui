b7acb99bad40f515ffcc5c1c02444dd6fe4dea53
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-01 09:28:25 -0500

    add p and code components to mdx components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -20,6 +20,16 @@ export function useMDXComponents(components: MDXComponents) {
         {children}
       </h3>
     ),
+    p: ({ children }: React.ComponentProps<"p">) => (
+      <p className="mt-4 text-4 leading-7 text-foreground [&:where(h2+&)]:mt-3.5">
+        {children}
+      </p>
+    ),
+    code: ({ children }: React.ComponentProps<"code">) => (
+      <code className="-my-1 rounded-1 bg-muted p-1 font-mono text-muted-foreground">
+        {children}
+      </code>
+    ),
     pre: ({ children }: React.ComponentProps<"pre">) => {
       if (!children || typeof children !== "object" || !("props" in children)) {
         return null;
@@ -28,7 +38,7 @@ export function useMDXComponents(components: MDXComponents) {
       const props = children.props as { children: string };
 
       return (
-        <CodeBlock className="mt-4 [&:where(h2+&)]:mt-5">
+        <CodeBlock className="mt-4 [&:where(p+&)]:mt-6">
           {props.children}
         </CodeBlock>
       );
