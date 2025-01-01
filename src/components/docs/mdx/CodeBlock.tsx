cfefe43e34b8426639709bfcde24777a4c972b84
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-01 10:25:36 -0500

    add syntax highlighting

--- src/components/docs/mdx/CodeBlock.tsx
+++ src/components/docs/mdx/CodeBlock.tsx
@@ -1,3 +1,5 @@
+import { highlight } from "sugar-high";
+
 import { cn } from "~/utils/tailwind";
 
 import CopyToClipboardButton from "~/components/docs/mdx/ClipboardButton";
@@ -8,12 +10,15 @@ interface CodeBlockProps {
 }
 
 export default function CodeBlock({ children, className }: CodeBlockProps) {
+  const highlightedCode = highlight(children);
+
   return (
     <div className={cn("relative rounded-2.5 border bg-accent/70", className)}>
       <pre className="max-h-112 overflow-auto overscroll-contain p-4">
-        <code className="text-3.5 leading-6 text-foreground/70">
-          {children}
-        </code>
+        <code
+          className="text-3.5 leading-6 text-foreground/70"
+          dangerouslySetInnerHTML={{ __html: highlightedCode }}
+        />
       </pre>
       <CopyToClipboardButton text={children} />
     </div>
