ef43e459c9aa4f7b59f66011280da7a2c8db498c
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-02 13:53:15 -0500

    fix docs toc scrolling

--- src/components/docs/navigation/DocsTOC.tsx
+++ src/components/docs/navigation/DocsTOC.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "~/utils/tailwind";
 import type { DocsTOCItem } from "~/types/docs";
 import useActiveId from "~/hooks/useActiveId";
+import { ScrollArea, ScrollBar } from "~/components/ui/ScrollArea";
 
 interface DocsTOCProps {
   tocs: Record<string, DocsTOCItem[]>;
@@ -24,24 +25,27 @@ export default function DocsTOC({ tocs }: DocsTOCProps) {
 
   return (
     <div className="hidden w-58 shrink-0 pr-4 xl:block">
-      <div className="fixed bottom-0 top-28 w-full">
-        <nav className="flex flex-col items-start gap-y-2.5">
-          {toc.map((item) => (
-            <Link
-              href={`#${item.id}`}
-              className={cn(
-                "text-3.5 focus-visible:outline-none",
-                item.id === activeId
-                  ? "font-medium text-foreground"
-                  : "text-muted-foreground hover:text-foreground focus-visible:text-foreground",
-                item.type === "h3" && "ml-6",
-              )}
-              key={item.id}
-            >
-              {item.text}
-            </Link>
-          ))}
-        </nav>
+      <div className="fixed bottom-0 top-19 w-full">
+        <ScrollArea className="h-full">
+          <nav className="flex flex-col items-start gap-y-2.5 py-9">
+            {toc.map((item) => (
+              <Link
+                href={`#${item.id}`}
+                className={cn(
+                  "text-3.5 focus-visible:outline-none",
+                  item.id === activeId
+                    ? "font-medium text-foreground"
+                    : "text-muted-foreground hover:text-foreground focus-visible:text-foreground",
+                  item.type === "h3" && "ml-6",
+                )}
+                key={item.id}
+              >
+                {item.text}
+              </Link>
+            ))}
+          </nav>
+          <ScrollBar className="bg-transparent" />
+        </ScrollArea>
       </div>
     </div>
   );
