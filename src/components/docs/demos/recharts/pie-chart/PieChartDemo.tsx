0b46ce6b961394c83f6920a527b42de3255bcb79
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-07 14:08:47 -0500

    add pie chart demo

--- src/components/docs/demos/recharts/pie-chart/PieChartDemo.tsx
+++ src/components/docs/demos/recharts/pie-chart/PieChartDemo.tsx
@@ -1,3 +1,58 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/components/ui/Chart";
+
+const CHART_DATA = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
 export default function PieChartDemo() {
-  return <></>;
+  return (
+    <ChartContainer config={chartConfig} className="min-h-64 w-full max-w-96">
+      <PieChart>
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Pie data={CHART_DATA} dataKey="visitors" nameKey="browser" />
+      </PieChart>
+    </ChartContainer>
+  );
 }
