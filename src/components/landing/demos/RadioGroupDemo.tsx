5aaf3ff697245bae00935ec85970784a1ef6f2e6
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-08 08:14:39 -0500

    add date picker, input, radio group demos to landing

--- src/components/landing/demos/RadioGroupDemo.tsx
+++ src/components/landing/demos/RadioGroupDemo.tsx
@@ -1,3 +1,21 @@
+import { Label } from "~/components/ui/Label";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/RadioGroup";
+
 export default function RadioGroupDemo() {
-  return <div>RadioGroupDemo</div>;
+  return (
+    <RadioGroup defaultValue="comfortable" className="z-10">
+      <div className="flex items-center gap-x-2">
+        <RadioGroupItem value="default" id="r1" />
+        <Label htmlFor="r1">Default</Label>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <RadioGroupItem value="comfortable" id="r2" />
+        <Label htmlFor="r2">Comfortable</Label>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <RadioGroupItem value="compact" id="r3" />
+        <Label htmlFor="r3">Compact</Label>
+      </div>
+    </RadioGroup>
+  );
 }
