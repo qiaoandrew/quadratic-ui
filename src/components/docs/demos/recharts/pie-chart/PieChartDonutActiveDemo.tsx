2e330899e9e454582331201b65f2bc7cfdf9bc81
Andrew Qiao <andrewqiao2004@gmail.com>
2025-01-08 18:50:51 -0500

    add pie chart demos

--- src/components/docs/demos/recharts/pie-chart/PieChartDonutActiveDemo.tsx
+++ src/components/docs/demos/recharts/pie-chart/PieChartDonutActiveDemo.tsx
@@ -1,3 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import { Pie, PieChart, Sector } from "recharts";
+import type { PieSectorDataItem } from "recharts/types/polar/Pie";
+
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/components/ui/Chart";
+
+const CHART_DATA = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const CHART_CONFIG = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
 export default function PieChartDonutActiveDemo() {
-  return <div>PieChartDonutActiveDemo</div>;
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  return (
+    <ChartContainer config={CHART_CONFIG} className="min-h-64 w-full max-w-96">
+      <PieChart>
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Pie
+          data={CHART_DATA}
+          dataKey="visitors"
+          nameKey="browser"
+          innerRadius={60}
+          strokeWidth={5}
+          activeIndex={activeIndex}
+          activeShape={({ outerRadius = 0, ...props }: PieSectorDataItem) => (
+            <Sector {...props} outerRadius={outerRadius + 10} />
+          )}
+          onClick={(_, index) => setActiveIndex(index)}
+        />
+      </PieChart>
+    </ChartContainer>
+  );
 }
--- src/components/docs/demos/recharts/pie-chart/PieChartLegendDemo.tsx
+++ src/components/docs/demos/recharts/pie-chart/PieChartLegendDemo.tsx
@@ -1,3 +1,58 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+} from "~/components/ui/Chart";
+
+const CHART_DATA = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const CHART_CONFIG = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
 export default function PieChartLegendDemo() {
-  return <div>PieChartLegendDemo</div>;
+  return (
+    <ChartContainer config={CHART_CONFIG} className="min-h-64 w-full max-w-96">
+      <PieChart>
+        <Pie data={CHART_DATA} dataKey="visitors" />
+        <ChartLegend
+          content={<ChartLegendContent nameKey="browser" />}
+          className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+        />
+      </PieChart>
+    </ChartContainer>
+  );
 }
